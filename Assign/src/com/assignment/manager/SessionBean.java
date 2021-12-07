5e14005911753992be023ecc47de1bf46cb320c5
mazziFCB <72908580+mazziFCB@users.noreply.github.com>
2021-12-07 16:21:20 +0200

    Delete Assign directory

--- Assign/src/com/assignment/manager/SessionBean.java
+++
@@ -1,8 +0,0 @@
-package com.assignment.manager;
-
-import javax.faces.bean.ManagedBean;
-
-@ManagedBean
-public class SessionBean {
-
-}
--- Assign/src/com/assignment/utils/Constant.java
+++
@@ -1,9 +0,0 @@
-package com.assignment.utils;
-
-
-public class Constant {
-
-	public static final int MAX_INTERVAL_TIME_OUT = 300;
-	public static final String DATABASE_URL = "jdbc:ucanaccess://E:/accountsdb.accdb";
-	
-}
